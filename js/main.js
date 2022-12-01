1098bd52fe892fcb15e8de4140330a13ede7f99f
Erik Olson <oerik100@gmail.com>
2022-11-30 22:09:48 -0500

    made changes to the function trying to get more definitions

--- js/main.js
+++ js/main.js
@@ -32,20 +32,26 @@ document.querySelector('#nextDefinition').addEventListener('click', getNextDefin
 
 function getNextDefinition(){
   const choice = document.querySelector('input').value
-  console.log(choice)
+  // console.log(choice)
   const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${choice}`
-
-
+  
+let i = 0
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-
-      for(let i = 1; i < data.length; i++) {
-        document.querySelector('#definition1').innerText = data[i].meanings[i].definitions[i].definition
-      }
-
+        // console.log(data)
+        const len = data[0].meanings[0].definitions.length
+        // console.log(len)
+        
+      // document.querySelector('#definition1').innerText = data[1].meanings[1].definitions[0].definition
+        if(i < len) {
+          document.querySelector('#definition1').innerText = data[0].meanings[0].definitions[i].definition
+          i = Number(i) + 1
+          console.log(i)
+        }
       })
+
+
     }
 
 
